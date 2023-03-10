1b3475b65b4bb33a5a1f8d8ff78eaa8041a8e63e
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-03-10 22:09:08 +0100

    added function for binding events, added changable task toggle button, edited css

--- js/script.js
+++ js/script.js
@@ -1,46 +1,7 @@
 {
     const tasks = [];
 
-    const addNewTask = (newTaskName) => {
-        tasks.push(
-            {
-                name: newTaskName,
-                done: false
-            }
-        )
-        render();
-        clearFocus();
-    };
-
-    const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
-        render();
-    };
-
-    const checkTask = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
-        render();
-    };
-
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            let taskStatus = "";
-
-            task.done === true ? taskStatus = "taskList__taskName--done" : "";
-
-            htmlString += `
-            <li class="taskList__task">
-            <button class="taskList__button js-checkButton">&#10004;</button>
-            <span class="taskList__taskName ${taskStatus}">${task.name}</span>
-            <button class="taskList__button taskList__button--remove js-removeButton">&#128465;</button>
-            </li>
-            `;
-        };
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-removeButton")
         removeButtons.forEach((removeButton, taskIndex) => {
             removeButton.addEventListener("click", () => {
@@ -51,8 +12,47 @@
         const checkButtons = document.querySelectorAll(".js-checkButton")
         checkButtons.forEach((checkButton, taskIndex) => {
             checkButton.addEventListener("click", () =>
-                checkTask(taskIndex));
+                toggleTaskDone(taskIndex));
         });
+    };
+
+    const addNewTask = (newTaskName) => {
+        tasks.push(
+            {
+                name: newTaskName,
+                done: false
+            }
+        )
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+
+            htmlString += `
+            <li class="taskList__task">
+                <button class="taskList__button ${task.done === true? "taskList__button--checked" : ""} js-checkButton">&#10004;</button>
+                <span class="taskList__taskName ${task.done === true ? "taskList__taskName--done" : ""}">${task.name}</span>
+                <button class="taskList__button taskList__button--remove js-removeButton">&#128465;</button>
+            </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
     const onFormSubmit = () => {
@@ -60,16 +60,16 @@
 
         const newTaskName = document.querySelector(".js-newTask").value.trim();
 
-        if (newTaskName === "") {
-            clearFocus();
-            return;
+        if (newTaskName !== "") {
+            addNewTask(newTaskName);
         };
 
-        addNewTask(newTaskName);
+        resetInput();
     };
 
-    const clearFocus = () => {
+    const resetInput = () => {
         const taskInput = document.querySelector(".js-newTask");
+
         taskInput.value = "";
         taskInput.focus();
     };
